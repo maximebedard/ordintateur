b45038435f6e9bbec96441cd26a7eb7e0f5d83bc
Maxime Bedard <maxime.bedard@shopify.com>
2017-03-25 20:58:50 -0400

    assing user to video :

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -6,6 +6,7 @@ class VideosController < ApplicationController
 
   def show
     @video = Video.find(params[:id])
+    @feedback = Ordinateur.redis.hget("likes:#{current_user.id}")
   end
 
   def new
@@ -13,18 +14,18 @@ class VideosController < ApplicationController
   end
 
   def create
-    @video = Video.new(video_params)
-    @video.video_url = upload_video(params[:video][:io]) if @video.valid?
-    @video.save
-
+    @video = Video.new
+    update_video
     respond_with(@video)
+  end
+
+  def like
+    Ordinateur.redis.hset("likes:#{current_user.id}", !!params[:feedback])
   end
 
   def update
     @video = Video.find(params[:id])
-    @video.assign_attributes(video_params)
-    @video.video_url = upload_video(params[:video][:io]) if @video.valid?
-    @video.save
+    update_video
 
     respond_with(@video)
   end
@@ -44,8 +45,18 @@ class VideosController < ApplicationController
 
   private
 
+  def update_video
+    io = params[:video].delete(:io)
+    tags = params[:video].delete(:tags)
+    @video.assign_attributes(video_params)
+    @video.video_url = upload_video(io)
+    @video.user = current_user
+    @video.tags = Array(tags.split(","))
+    @video.save
+  end
+
   def video_params
-    params.require(:video).permit(:titre, :description, :tags)
+    params.require(:video).permit(:titre, :description, :io, :tags)
   end
 
   def upload_video(uploaded_io)
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,8 +1,14 @@
 class Video < ApplicationRecord
   has_many :comments
+  belongs_to :user
   validates :titre, :description, :video_url, presence: true
 
   def public_url
     ""
+  end
+
+  def views_count
+    # shitty but works
+    Ordinateur.redis.increment("views:#{@video.id}:#{current_user.id}")
   end
 end
