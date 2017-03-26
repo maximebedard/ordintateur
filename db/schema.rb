733ff5dcece5d4f941dc46fe4ee581c7331140d0
Maxime Bedard <maxime.bedard@shopify.com>
2017-03-25 21:03:38 -0400

    regen schema rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170326003554) do
 
   # These are extensions that must be enabled in order to support this database
@@ -49,9 +48,10 @@ ActiveRecord::Schema.define(version: 20170326003554) do
     t.string   "titre"
     t.text     "description"
     t.string   "video_url"
+    t.integer  "user_id"
     t.text     "tags",        default: [],              array: true
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
 end
